874b01a917f9c4499ba59adee3f22ede884b35cb
Hansel <hanselcapillo@hotmail.com>
2019-08-30 17:51:01 -0500

    se agrega prueba de error 500

--- server/server.spec.js
+++ server/server.spec.js
@@ -40,8 +40,21 @@ describe('Testing Router', () => {
                 }]);
                 done();
             })
-        })
+        });
 
         // GET 500
+        it('500', done => {
+            const data = [{
+                id: 1
+            }];
+            spyOn(Pins, 'find').and.callFake(callBack => {
+                callBack(true, data);
+            });
+
+            request.get('http://localhost:3000/api', (error, response, body) => {
+                expect(response.statusCode).toBe(500);
+                done();
+            });
+        });
     })
 })
