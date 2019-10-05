63f6e7f1cdcfb65ec50b69de4a63a299ed0e9b52
Hansel <hanselcapillo@hotmail.com>
2019-10-05 17:32:12 -0400

    Trabajando con event emitters

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,16 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { MenuComponent } from './menu.component';
+import { MenuComponent } from "./menu.component";
+import { By } from "@angular/platform-browser";
 
-describe('MenuComponent', () => {
+fdescribe("MenuComponent", () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
+      declarations: [MenuComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +19,19 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should create", () => {
+    const title = fixture.debugElement.query(By.css("h1"));
+
+    expect(title.nativeElement.innerHTML).toBe("eLearning Management System");
+  });
+
+  it("Testing output", () => {
+    const val = true;
+
+    component.clicked.subscribe(result => {
+      expect(result).toBe(val);
+    });
+
+    component.clicked.next(val);
   });
 });
